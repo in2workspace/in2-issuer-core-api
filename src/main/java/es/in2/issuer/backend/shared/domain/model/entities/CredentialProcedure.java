2a1128da631a5490a61486093a4c521c8ae0fa5e
Roger Miret <roger.miret@in2.es>
2026-04-10 09:00:26 +0200

    remove unused import

--- src/main/java/es/in2/issuer/backend/shared/domain/model/entities/CredentialProcedure.java
+++ src/main/java/es/in2/issuer/backend/shared/domain/model/entities/CredentialProcedure.java
@@ -8,7 +8,6 @@ import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.sql.Timestamp;
-import java.time.Instant;
 import java.util.UUID;
 
 @Getter
